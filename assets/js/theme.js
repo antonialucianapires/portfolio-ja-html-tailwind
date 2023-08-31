e261d67150802807dc75976def207d370a2d2411
antonialucianapires <antonialucianapires@gmail.com>
2023-08-31 04:12:59 -0300

    feat: ctiação da seção projetos +conexao github api

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,7 +4,10 @@ const body = document.body;
 const header = document.querySelector('header');
 const brandName = document.getElementById('brandName');
 const navLinks = document.getElementById('navLinks');
-const socialIcons = document.getElementById('socialIcons'); 
+const cards = document.querySelectorAll('.card'); 
+const loadMoreButton = document.getElementById('loadMore'); 
+const skillTitles = document.querySelectorAll('.skill-title'); 
+const skillDescriptions = document.querySelectorAll('.skill-description'); 
 
 themeToggle.addEventListener('click', () => {
   if (body.classList.contains('bg-white')) {
@@ -12,14 +15,35 @@ themeToggle.addEventListener('click', () => {
     header.className = 'bg-gray-800 text-white';
     brandName.className = 'text-2xl font-bold text-white';
     navLinks.className = 'text-lg space-x-4 text-white';
-    socialIcons.className = 'w-full sm:w-full md:w-full lg:w-32 flex flex-row sm:flex-row md:flex-row lg:flex-col items-center justify-center bg-transparent mb-8 space-x-4 sm:space-x-4 md:space-x-4 lg:space-x-0 space-y-0 sm:space-y-0 md:space-y-0 lg:space-y-2 text-white'; // Nova linha
     themeIcon.className = 'fas fa-sun';
+
+
+    cards.forEach(card => {
+      card.className = 'card bg-white border-black text-black';
+    });
+    loadMoreButton.className = 'bg-purple-600 text-white px-4 py-2 rounded mt-4 mx-auto';
+    skillTitles.forEach(title => {
+      title.className = 'skill-title text-white font-bold relative ml-2';
+    });
+    skillDescriptions.forEach(description => {
+      description.className = 'skill-description text-xs text-white mb-3';
+    });
   } else {
     body.className = 'bg-white text-gray-900';
     header.className = 'bg-white text-gray-900';
     brandName.className = 'text-2xl font-bold text-gray-900';
     navLinks.className = 'text-lg space-x-4 text-gray-900';
-    socialIcons.className = 'w-full sm:w-full md:w-full lg:w-32 flex flex-row sm:flex-row md:flex-row lg:flex-col items-center justify-center bg-transparent mb-8 space-x-4 sm:space-x-4 md:space-x-4 lg:space-x-0 space-y-0 sm:space-y-0 md:space-y-0 lg:space-y-2 text-gray-600'; // Nova linha
     themeIcon.className = 'fas fa-moon';
+
+    cards.forEach(card => {
+      card.className = 'card';
+    });
+    loadMoreButton.className = 'bg-gray-400 text-white px-4 py-2 rounded mt-4 mx-auto';
+    skillTitles.forEach(title => {
+      title.className = 'skill-title text-black font-bold relative ml-2';
+    });
+    skillDescriptions.forEach(description => {
+      description.className = 'skill-description text-xs text-white mb-3';
+    });
   }
 });
