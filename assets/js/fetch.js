5a7d0208e88da472182ca3768ec71e3de4295059
antonialucianapires <antonialucianapires@gmail.com>
2023-08-31 05:19:48 -0300

    fix: ajustes na fonte

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,9 +1,24 @@
 let repos = [];
 let lastIndex = 0;
 
+// Mock data for local development
+const mockRepos = [
+    { name: "Repo1", language: "JavaScript", description: "This is a mock repo" },
+    { name: "Repo2", language: "Python", description: "This is another mock repo" },
+    { name: "Repo3", language: "Java", description: "Java mock repo" },
+    { name: "Repo4", language: "C++", description: "C++ mock repo" },
+    { name: "Repo5", language: "Ruby", description: "Ruby mock repo" },
+    { name: "Repo6", language: "Go", description: "Go mock repo" }
+];
+
 async function fetchGitHubRepos() {
-    const response = await fetch('https://api.github.com/users/juliarodriguesamaral/repos');
-    repos = await response.json();
+    // Check if running on localhost
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        repos = mockRepos;
+    } else {
+        const response = await fetch('https://api.github.com/users/juliarodriguesamaral/repos');
+        repos = await response.json();
+    }
     displayRepos();
 }
 
@@ -14,9 +29,9 @@ function displayRepos() {
     nextRepos.forEach((repo) => {
         const card = `
             <div class="card rounded shadow-lg border border-black p-4 rounded w-full sm:bg-white sm:text-black md:bg-white md:text-black" id="card1">
-                <h2 class="text-xl font-bold card-title">${repo.name}</h2>
-                <i class="fab fa-${repo.language ? repo.language.toLowerCase() : 'code'} card-icon"></i>
-                <p class="text-sm mt-2 card-description">${repo.description || 'Sem descrição'}</p>
+                <h2 class="text-xl font-bold card-title md:text-2xl">${repo.name}</h2>
+                <i class=" sm:text-2xl md:text-2xl fab fa-${repo.language ? repo.language.toLowerCase() : 'code'} card-icon"></i>
+                <p class="text-sm mt-2 card-description sm:text-2xl md:text-2xl">${repo.description || 'Sem descrição'}</p>
             </div>
         `;
         projectsDiv.innerHTML += card;
